d5a02cfc109361281f2e65651fe356d881e8aa50
kriscfoster <kfoster@salesforce.com>
2020-10-30 08:46:44 +0000

    multiple observers

--- src/com/kriscfoster/TaxMan.java
+++ src/com/kriscfoster/TaxMan.java
@@ -1,13 +1,14 @@
 package com.kriscfoster;
 
-public class Payroll implements Observer {
+public class TaxMan implements Observer {
 
     @Override
     public void update(Subject subject) {
         Employee employee = (Employee) subject;
         String string = employee.getName() +
-                " salary is now " +
-                employee.getSalary();
+                " needs to pay " +
+                employee.getSalary() * 0.2 +
+                " in taxes";
         System.out.println(string);
     }
 }
